f1a6d0f15cecd8af6cbdd38813aaa82f64263ba9
Tim Smart <tim.smart@arisechurch.com>
2022-12-13 17:08:23 +1300

    chore: seperate express in example

--- packages/router/examples/src/server.ts
+++ packages/router/examples/src/server.ts
@@ -1,6 +1,4 @@
 import { Server } from "@effect-rpc/router"
-import Express from "express"
-import { Effect, Exit } from "./common.js"
 import * as One from "./one.js"
 import * as Two from "./two.js"
 
@@ -9,22 +7,7 @@ export const schema = {
   ...Two.routes,
 }
 
-const handle = Server.make(schema, {
+export const handler = Server.make(schema, {
   ...One.handlers,
   ...Two.handlers,
 })
-
-const app = Express()
-app.use(Express.json())
-app.post("/api", (req, res) =>
-  Effect.unsafeRunAsyncWith(handle(req.body), (exit) => {
-    if (Exit.isSuccess(exit)) {
-      res.send(exit.value)
-    } else {
-      res.writeHead(500)
-      res.end()
-    }
-  }),
-)
-
-app.listen(3000)
