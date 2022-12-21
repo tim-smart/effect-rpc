1f16dc9eb680c18f50016d9a736a47deea08ca99
Tim Smart <tim.smart@arisechurch.com>
2022-12-22 11:43:31 +1300

    feat: HubRef map

--- packages/react/src/HubRef.ts
+++ packages/react/src/HubRef.ts
@@ -2,19 +2,30 @@ import * as Effect from "@effect/io/Effect"
 import * as Exit from "@effect/io/Exit"
 import * as FiberId from "@effect/io/Fiber/Id"
 import * as Hub from "@effect/io/Hub"
-import { Dequeue } from "@effect/io/Queue"
 import * as Ref from "@effect/io/Ref"
 import * as Scope from "@effect/io/Scope"
 import { pipe } from "@fp-ts/data/Function"
 import { useEffect, useMemo, useState } from "react"
 
-export interface HubRef<A> {
-  get: Effect.Effect<never, never, A>
-  set: (a: A) => Effect.Effect<never, never, void>
-  update: (f: (a: A) => A) => Effect.Effect<never, never, void>
-  subscribe: Effect.Effect<Scope.Scope, never, Dequeue<A>>
+/**
+ * @tsplus type effect-rpc/react/HubRef.ReadOnly
+ */
+export interface ReadOnlyHubRef<R, E, A> {
+  get: Effect.Effect<R, E, A>
+  subscribe: Effect.Effect<Scope.Scope, never, Effect.Effect<R, E, A>>
 }
 
+/**
+ * @tsplus type effect-rpc/react/HubRef
+ */
+export interface HubRef<A> extends ReadOnlyHubRef<never, never, A> {
+  set: (a: A) => Effect.Effect<never, never, void>
+  update: (f: (a: A) => A) => Effect.Effect<never, never, void>
+}
+
+/**
+ * @tsplus static effect-rpc/react/HubRef make
+ */
 export const make = <A>(
   initialValue: A,
 ): Effect.Effect<never, never, HubRef<A>> =>
@@ -38,7 +49,10 @@ export const make = <A>(
           Effect.flatMap((a) => set(f(a))),
         )
 
-      const subscribe = Hub.subscribe(hub)
+      const subscribe = pipe(
+        Hub.subscribe(hub),
+        Effect.map((q) => pipe(q.take())),
+      )
 
       return {
         get,
@@ -49,7 +63,34 @@ export const make = <A>(
     }),
   )
 
-export const useHubRef = <A>(ref: HubRef<A>) => {
+/**
+ * @tsplus pipeable effect-rpc/react/HubRef.ReadOnly mapEffect
+ */
+export const mapEffect =
+  <R, E, A, B>(f: (a: A) => Effect.Effect<R, E, B>) =>
+  <R1, E1>(
+    self: ReadOnlyHubRef<R1, E1, A>,
+  ): ReadOnlyHubRef<R | R1, E | E1, B> => {
+    const get = pipe(self.get, Effect.flatMap(f))
+
+    const subscribe = pipe(
+      self.subscribe,
+      Effect.map((take) => pipe(take, Effect.flatMap(f))),
+    )
+
+    return { get, subscribe }
+  }
+
+/**
+ * @tsplus pipeable effect-rpc/react/HubRef.ReadOnly map
+ */
+export const map = <A, B>(f: (a: A) => B) =>
+  mapEffect((a: A) => Effect.succeed(f(a)))
+
+/**
+ * @tsplus getter effect-rpc/react/HubRef use
+ */
+export const useHubRef = <A>(ref: ReadOnlyHubRef<never, never, A>) => {
   // Current value
   const [value, setValue] = useState(() => Effect.unsafeRunSync(ref.get))
 
@@ -66,9 +107,9 @@ export const useHubRef = <A>(ref: HubRef<A>) => {
   useEffect(() => {
     const effect = pipe(
       Scope.use(ref.subscribe)(scope),
-      Effect.flatMap((q) =>
+      Effect.flatMap((take) =>
         pipe(
-          q.take(),
+          take,
           Effect.tap((a) =>
             Effect.sync(() => {
               setValue(a)
